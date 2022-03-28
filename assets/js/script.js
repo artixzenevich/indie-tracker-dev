c1a0b97d22308fd87d6318862b7c35b7b4af80b3
Artik Zenevich <azenevich91@gmail.com>
2022-03-28 20:16:21 +0300

    redesign main page

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,23 +2,34 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const meta = document.querySelectorAll('.metacritic__metascore');
     const scrollup = document.querySelector('.scrollup');
+    const navbar = document.querySelector('.nav-header');
+
+    var sticky = navbar.offsetTop;
 
     metascoreColors();
 
+    window.addEventListener('scroll', navbarSticky);
     window.addEventListener('scroll', trackScroll);
     scrollup.addEventListener('click', backToTop);
 
-    function trackScroll() {
-        var scrolled = window.pageYOffset;
-        var coords = document.documentElement.clientHeight;
-    
-        if (scrolled > coords) {
-          scrollup.classList.add('show');
-        }
-        if (scrolled < coords) {
-          scrollup.classList.remove('show');
-        }
+    function navbarSticky() {
+      if (window.pageYOffset >= sticky) {
+        navbar.classList.add('nav-header--sticky');
+      } else {
+        navbar.classList.remove('nav-header--sticky');
       }
+    }
+
+    function trackScroll() {
+      var scrolled = window.pageYOffset;
+      var coords = document.documentElement.clientHeight;
+      if (scrolled > coords) {
+        scrollup.classList.add('show');
+      }
+      if (scrolled < coords) {
+        scrollup.classList.remove('show');
+      }
+    }
 
     function backToTop() {
         if (window.pageYOffset > 0) {
--- assets/js/slider.js
+++ assets/js/slider.js
@@ -1,7 +1,16 @@
 $(document).ready(function(){
     $('.slider').slick({
-        dots: true,
+        arrows: false,
         autoplay: true,
         autoplaySpeed: 5000,
+        asNavFor: '.slider-nav'
+    });
+    $('.slider-nav').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        arrows: false,
+        asNavFor: '.slider',
+        centerMode: true,
+        focusOnSelect: true
     });
 })
